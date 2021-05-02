538eede45817cd05745b7aff9513bc74a898e77d
Jason Birbal <skydom@zoho.com>
2021-05-02 11:36:57 +0300

    Added css style sheet

--- html/wp-content/plugins/WP-customAPI/wp-vue.php
+++ html/wp-content/plugins/WP-customAPI/wp-vue.php
@@ -5,15 +5,26 @@
  * Description: Vue-App in WordPress.
  */
 
-/* function func_load_vuescripts()
+function func_load_vuescripts()
 {
-    wp_register_script('wpvue_vuejs', plugin_dir_url(__FILE__) . 'dist/js/app.c8d5a15f.js', true);
-    wp_register_script('wpvue_vuejs1', plugin_dir_url(__FILE__) . 'dist/js/chunk-vendors.5e0c61d5.js', true);
-    wp_register_script('wpvue_vuejs2', plugin_dir_url(__FILE__) . 'html\wp-content\plugins\WP-customAPI\dist\js\about.ea5fbbe6.js', true);
-    wp_register_script('wpvue_vuejs3', plugin_dir_url(__FILE__) . 'html\wp-content\plugins\WP-customAPI\dist\js\app.fb10eec2.js', true);
-    wp_register_script('wpvue_vuejs4', plugin_dir_url(__FILE__) . 'html\wp-content\plugins\WP-customAPI\dist\js\chunk-vendors.beae7116.js', true);
-    wp_register_script('wpvue_vuejs5', plugin_dir_url(__FILE__) . 'html\wp-content\plugins\WP-customAPI\dist\precache-manifest.bd64e30f15aac3de68d8094ee57dcb91.js', true);
-    wp_register_script('wpvue_vuejs6', plugin_dir_url(__FILE__) . 'html\wp-content\plugins\WP-customAPI\dist\service-worker.js', true);
+    wp_register_script(
+        'app',
+        plugin_dir_url(__FILE__) . 'dist/js/app.js',
+
+        true,
+    );
+    wp_register_script(
+        'vendors',
+        plugin_dir_url(__FILE__) . 'dist/js/chunk-vendors.js',
+
+        true,
+    );
+    /*     wp_register_script(
+        'about',
+        plugin_dir_url(__FILE__) . 'dist/js/about.js',
+
+        true,
+    ); */
 }
 
 add_action('wp_enqueue_scripts', 'func_load_vuescripts');
@@ -21,27 +32,18 @@ add_action('wp_enqueue_scripts', 'func_load_vuescripts');
 //Add shortscode
 function func_wp_vue()
 {
-    wp_enqueue_script('wpvue_vuejs');
-    wp_enqueue_script('wpvue_vuejs1');
-    wp_enqueue_script('wpvue_vuejs2');
-    wp_enqueue_script('wpvue_vuejs3');
-    wp_enqueue_script('wpvue_vuejs4');
-    wp_enqueue_script('wpvue_vuejs5');
-    wp_enqueue_script('wpvue_vuejs6');
+    wp_enqueue_script('app');
+    wp_enqueue_script('vendors');
+    // wp_enqueue_script('about');
+
+
 
     $str = "<div id='app'>"
         . "Message from Vue: "
         . "</div>";
     return $str;
-} // end function */
+} // end function 
 
 //Add shortscode
-function func_wp_vue()
-{
-    $str = "<div id='divWpVue'>"
-        . "Message from Vue: "
-        . "</div>";
-    return $str;
-} // end function
 
 add_shortcode('wpvue', 'func_wp_vue');
