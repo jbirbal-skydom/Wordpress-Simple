ea20a763d821d35c80a312e91cb5af646e22b2de
Jason Birbal <skydom@zoho.com>
2021-06-12 02:03:55 +0300

    Create new Vol for each, Connect each WP site, Shared DB
    Todo;
    - landing page

---
+++ add_users.php
@@ -0,0 +1,5 @@
+define('CUSTOM_USER_TABLE', 'landing_users');
+define('CUSTOM_USERMETA_TABLE', 'landing_usermeta');
+
+define('COOKIE_DOMAIN', '.skydom.localhost'); //replace with the 1st website url
+define('COOKIEPATH', '/');
