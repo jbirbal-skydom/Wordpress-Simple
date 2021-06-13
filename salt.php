84ada2ca38e8871863f2595761a7b8a274d9166a
Jason Birbal <skydom@zoho.com>
2021-06-13 21:03:00 +0300

    Nginx
    toDo:
    cross site login

---
+++ salt.php
@@ -0,0 +1,30 @@
+/* // Original
+*define( 'AUTH_KEY', getenv_docker('WORDPRESS_AUTH_KEY', 'b3236b6c1a20eae22909d0845649e8b2fff1d40e') );
+*define( 'SECURE_AUTH_KEY', getenv_docker('WORDPRESS_SECURE_AUTH_KEY', 'deb0b228439e784383b65e1dfe5c35e6b4df62e2') );
+*define( 'LOGGED_IN_KEY', getenv_docker('WORDPRESS_LOGGED_IN_KEY', '7fe90e7eaa53a35fe4378443b3b83f0a0c273516') );
+*define( 'NONCE_KEY', getenv_docker('WORDPRESS_NONCE_KEY', 'c63c2e0e89cc9948329e0db73482e6b4ce4cc0e6') );
+*define( 'AUTH_SALT', getenv_docker('WORDPRESS_AUTH_SALT', 'b35cb7ba5de86e1b0c1e28db2d1fdd449bc12edd') );
+*define( 'SECURE_AUTH_SALT', getenv_docker('WORDPRESS_SECURE_AUTH_SALT', 'f51964f6a5b16bb3384eacc4f7535865ffb5f3cf') );
+*define( 'LOGGED_IN_SALT', getenv_docker('WORDPRESS_LOGGED_IN_SALT', '39e4f5bfd5f5883eacc2737c5992bed42ca136e9') );
+*define( 'NONCE_SALT', getenv_docker('WORDPRESS_NONCE_SALT', '91fd9f131ce265a34cfd6c5ddab388c4c4bd736e') );
+*/
+
+
+wordpress_logged_in_cffb670352ad40cd796ad890d27ee701:"andrewgrant%7C1623676168%7Cn0MMPUeXxBsXasfubacBJqbXenU3Vx0tG4vww7EF1TA%7C12f4b93397d3c6f8a062492c58fa4c355d56feebafbd46dd52900627115c1310"
+wordpress_logged_in_cffb670352ad40cd796ad890d27ee701:"andrewgrant%7C1623676168%7Cn0MMPUeXxBsXasfubacBJqbXenU3Vx0tG4vww7EF1TA%7C12f4b93397d3c6f8a062492c58fa4c355d56feebafbd46dd52900627115c1310"
+
+
+Action Encryption Key: 5UwCW8a97ZDz3vNLwf5k
+Action Signature Key: rRm8dYrmKbecVJQjPamJ
+
++---[RSA 3072]----+
+|. .o++  oo       |
+|....o*.o ..      |
+| ...= * o        |
+|   * * = .       |
+|  o B = S        |
+|   E . .         |
+|  o=o.  .        |
+| o=*=... .       |
+|.oOB+o. .        |
++----[SHA256]-----+
