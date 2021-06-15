aa588872b6661b5e75bb4a2d6a4273f517c43163
Jason Birbal <skydom@zoho.com>
2021-06-15 17:31:28 +0300

    Setup

--- salt.php
+++ salt.php
@@ -1,30 +1,27 @@
-/* // Original
-*define( 'AUTH_KEY', getenv_docker('WORDPRESS_AUTH_KEY', 'b3236b6c1a20eae22909d0845649e8b2fff1d40e') );
-*define( 'SECURE_AUTH_KEY', getenv_docker('WORDPRESS_SECURE_AUTH_KEY', 'deb0b228439e784383b65e1dfe5c35e6b4df62e2') );
-*define( 'LOGGED_IN_KEY', getenv_docker('WORDPRESS_LOGGED_IN_KEY', '7fe90e7eaa53a35fe4378443b3b83f0a0c273516') );
-*define( 'NONCE_KEY', getenv_docker('WORDPRESS_NONCE_KEY', 'c63c2e0e89cc9948329e0db73482e6b4ce4cc0e6') );
-*define( 'AUTH_SALT', getenv_docker('WORDPRESS_AUTH_SALT', 'b35cb7ba5de86e1b0c1e28db2d1fdd449bc12edd') );
-*define( 'SECURE_AUTH_SALT', getenv_docker('WORDPRESS_SECURE_AUTH_SALT', 'f51964f6a5b16bb3384eacc4f7535865ffb5f3cf') );
-*define( 'LOGGED_IN_SALT', getenv_docker('WORDPRESS_LOGGED_IN_SALT', '39e4f5bfd5f5883eacc2737c5992bed42ca136e9') );
-*define( 'NONCE_SALT', getenv_docker('WORDPRESS_NONCE_SALT', '91fd9f131ce265a34cfd6c5ddab388c4c4bd736e') );
-*/
+<!-- /* // Original
+define('AUTH_KEY',         'YQPf2!e/;}79GD6BED#9t( +=Nrt>3v.a&,.f~-b_5y<2>?D-NJ,DrOZwPCx6<>>');
+define('SECURE_AUTH_KEY',  'i8[_~:;#C7oE+nU`.GS/Ur%lVi)%>QZ&OAyJSDgZy/g|,7jG$^Vo3m],b+?X,ZF*');
+define('LOGGED_IN_KEY',    'OTyJ9u#||b~P:NU1kvWU&|.2+b![/h{<#?C~Q0?g-lJR.T@+sT4/h;*cjoQDtTum');
+define('NONCE_KEY',        'xr`W08|T|I-5.+O~6fb|Me~x#PWTR{LSa+]_1bv4.9G|o}<`PKZQmMj[Fo5BhScM');
+define('AUTH_SALT',        'NMuGTC79C5a;_fK,/K{D{8VV6d0Y#yZR~{WbBG)x@Xiuo7QH~/bhKV)9>u[.5oHV');
+define('SECURE_AUTH_SALT', 'x4}y.v`}%d,PNp`ue+*,eyrbWQcEKL%@f! T2a;ILqtj~]L)+QL~*MtBMgCrj1zg');
+define('LOGGED_IN_SALT',   '`.3`cCsHIPmr UaESje[2t%=xo!{xE-?4r-1IxVcuP}2dWl(L,lvC>*3BbT|2{EA');
+define('NONCE_SALT',       'aRB*|ghSkr`H<fV]2kT1$PAH9i;1T ;}<MVCO{`]gP|p*E`W.v/xZ3_|xBd^~%H;');
+*/ -->
 
 
-wordpress_logged_in_cffb670352ad40cd796ad890d27ee701:"andrewgrant%7C1623676168%7Cn0MMPUeXxBsXasfubacBJqbXenU3Vx0tG4vww7EF1TA%7C12f4b93397d3c6f8a062492c58fa4c355d56feebafbd46dd52900627115c1310"
-wordpress_logged_in_cffb670352ad40cd796ad890d27ee701:"andrewgrant%7C1623676168%7Cn0MMPUeXxBsXasfubacBJqbXenU3Vx0tG4vww7EF1TA%7C12f4b93397d3c6f8a062492c58fa4c355d56feebafbd46dd52900627115c1310"
 
-
-Action Encryption Key: 5UwCW8a97ZDz3vNLwf5k
-Action Signature Key: rRm8dYrmKbecVJQjPamJ
+Action Encryption Key: 6&l `|m]/j+>~ `|syzRj;nb!vnaGEZkJ%kP(x3KnDty h+6HH;Pu~JWB=w +$2|
+Action Signature Key: ]u-xTw0RdPB032PG7WMpcCdt;~/]{x/-)SM`:j9`gac*7:|I$J-66~WO,n^3M%Z&
 
 +---[RSA 3072]----+
-|. .o++  oo       |
-|....o*.o ..      |
-| ...= * o        |
-|   * * = .       |
-|  o B = S        |
-|   E . .         |
-|  o=o.  .        |
-| o=*=... .       |
-|.oOB+o. .        |
+|. .o++ oo |
+|....o*.o .. |
+| ...= * o |
+| * * = . |
+| o B = S |
+| E . . |
+| o=o. . |
+| o=*=... . |
+|.oOB+o. . |
 +----[SHA256]-----+
